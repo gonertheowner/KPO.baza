4d952f1b268ace662023131730619c82f485c628
Kirill <gonertheowner@gmail.com>
2022-11-16 15:12:45 +0300

    Refactored code-style and data access methods

--- digitalSecretary/src/main/java/data_base/DataManager.java
+++ digitalSecretary/src/main/java/data_base/DataManager.java
@@ -4,7 +4,6 @@ import java.sql.*;
 
 
 public class DataManager {
-
     public static final Connection connection;
 
     static
@@ -15,77 +14,83 @@ public class DataManager {
             e.printStackTrace();
         }
 
-        try
-        {
+        try {
             connection = DriverManager.getConnection(ConfProperties.getProperty("db.URL"), ConfProperties.getProperty("db.login"), ConfProperties.getProperty("db.password"));
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static void selectAll(String tableName) throws SQLException {
+    public static boolean selectAll(String tableName) throws SQLException {
+        boolean flag;
         if (tableName == null) {
-            // trow some exception
+            flag = false;
+            // OurClassException
         }
-        var statement = connection.prepareStatement("SELECT * FROM " + tableName);
-        if (statement.execute()) {
-            System.out.println("Success!");
-        } else {
-            // throw some exception
-        }
+
+        var statement = connection.prepareStatement("SELECT * FROM ?");
+        statement.setString(1, tableName);
+        flag = statement.executeUpdate() != 0;
+        return flag;
     }
 
-    public static void addUser(User user) throws SQLException {
-        try
-        {
+    public static void addUser(User user) {
+        try {
             PreparedStatement preparedStatement = connection.prepareStatement("Insert Into users (login, password) VALUES (?,?)");
             preparedStatement.setString(1, user.getLogin());
             preparedStatement.setString(2, user.getPassword());
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
+            // OurClassException
             throw new RuntimeException(e);
         }
     }
-    public static void deleteUser(int index) throws SQLException {
-        try
-        {
-            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM users WHERE ID=?");
-            preparedStatement.setInt(1, index);
-            preparedStatement.executeUpdate();
+
+    public static boolean delete(String tableName, int index) {
+        boolean flag;
+        if (tableName == null) {
+            flag = false;
+            // OurClassException
         }
-        catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    public static void updateUser (int index,User user) {
+
         try {
-            PreparedStatement preparedStatement=connection.prepareStatement("UPDATE users SET login=?, password=? WHERE ID=?");
-            preparedStatement.setString(1,user.getLogin());
-            preparedStatement.setString(2,user.getPassword());
-            preparedStatement.setInt(3,index);
-            preparedStatement.executeUpdate();
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM ? WHERE ID=?");
+            preparedStatement.setString(1, tableName);
+            preparedStatement.setInt(2, index);
+            flag = preparedStatement.executeUpdate() != 0;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+        return flag;
     }
-    public static boolean searchUser(User user)
-    {
-        boolean flag=false;
-       try {
-           PreparedStatement preparedStatement = connection.prepareStatement("SELECT login FROM users WHERE EXISTS(SELECT login FROM users where login=?)");
-           preparedStatement.setString(1,user.getLogin());
-           var resultSet=preparedStatement.executeQuery();
-           if (resultSet.next()) {
-               flag=true;
-               System.out.println("есть такая строка");
-           }
-           else {
-               System.out.println("нет такой строки");
-           }
 
-       } catch (SQLException e) {
-           throw new RuntimeException(e);
-       }
+    public static boolean updateUser(int index, User user) {
+        boolean flag;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE users SET login=?, password=? WHERE ID=?");
+            preparedStatement.setString(1, user.getLogin());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setInt(3, index);
+            flag = preparedStatement.executeUpdate() != 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return flag;
+    }
+
+    public static boolean searchUser(User user) {
+        boolean flag = false;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT login FROM users WHERE EXISTS(SELECT login FROM users where login=?)");
+            preparedStatement.setString(1, user.getLogin());
+            var resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                flag = true;
+            }
+        } catch (SQLException e) {
+            // OurClassException
+            throw new RuntimeException(e);
+        }
         return flag;
     }
 }
